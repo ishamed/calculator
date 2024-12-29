3bd130f1134e1ab6ef50863af8aa849f5f87a06e
Mr Hamed <b24.hamed@gmail.com>
2024-12-29 12:36:00 +0330

    Add main java file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import controller.Calculator;
+import exceptions.ArithmeticError;
+import model.Equation;
+import view.Input;
+
+public class Main {
+
+    public static void main(String[] args){
+        Input.getInput().input();
+    }
+}
