9418c46f21c92c1c6a182f313338e2712bdf3164
Mr Hamed <b24.hamed@gmail.com>
2024-12-29 12:53:25 +0330

    Update run.bat to remove all .class files after excuting the program

--- run.bat
+++ run.bat
@@ -1,7 +1,18 @@
 @echo off
 cd src
+
 javac Main.java
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 (
+    echo Compilation failed. Cleaning up all .class files...
+    for /r %%i in (*.class) do del /q "%%i"
+    cd ..
+    exit /b %errorlevel%
+)
+
 java Main
+
+echo Cleaning up all .class files...
+for /r %%i in (*.class) do del /q "%%i"
+
 cd ..
 pause
